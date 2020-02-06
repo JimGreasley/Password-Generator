b30d154340c1ff6889cbd485b3aa90f0dc69c925
Jim Greasley <azjimgreasley@gmail.com>
2020-02-05 20:26:01 -0700

    Completed code to generate passwords.

--- README.md
+++ README.md
@@ -3,8 +3,13 @@
 A button click starts the process.
 
 The user is prompted for the length of the password to be created.
-The length is validated (must be between 8 and 128) and if not validated the user is re-prompted until a valid length is entered.
+The length is validated (must be between 8 and 128) and if not valid the user is re-prompted until a valid length is entered.
 With a valid length the user is next queried as what type of characters are to be used to create the password.
 The user must choose at least one of lowercase, uppercase, numeric or special characters. The user will be re-prompted if no option is chosen.
 
-Once we have a valid length and valid character type selection the super secure password will be generated and displayed on screen. 
+Once we have a valid length and valid character type selection the password will be generated.
+
+The new password starts as an empty string.  A function is used to determine the next random character, based on the user's selection criteria, to append to the new password.  Appending random characters continues until the new password
+reaches the desired length. 
+
+Once complete the new password is displayed on screen. Needed to change CSS for #password to use 'auto' overflow.
--- script.js
+++ script.js
@@ -4,14 +4,25 @@ var generateBtn = document.querySelector("#generate");
 // define variables used to generate password
 var lowercase_char  = "abcdefghijklmnopqrstuvwxyz";
 var uppercase_char  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var special_char    = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var numeric_char    = "1234567890";
 
+// Removed " \ _ and space(' ') from string of special characters
+var special_char    = "!#$%&'()*+,-./:;<=>?@[]^`{|}~";
 
 
+
+// ------------------------------------------------
 // function to create password based on user input
+// ------------------------------------------------
 
 function generatePassword() {
+
+  // -----------------------------------------------------
+  // clear previous password; code below doesn't work
+  // -----------------------------------------------------
+
+  // document.getElementById('passwrod').innerHTML = " ";
+
 
   // -------------------------------------------------------------------------------------
   // get desired length of password from user - must be greater than 7 and less than 129
@@ -31,23 +42,16 @@ function generatePassword() {
 
   console.log("Password length: ", pswd_length);
 
-  // ----------------------------------------------------------------------------------------------------------
-  // get password format criteria from user - include lowercase, uppercase, numeric and/or special caracters?
-  // ----------------------------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------------------------------------------
+  // get password format criteria from user - include lowercase, uppercase, numeric or special caracters
+  // ------------------------------------------------------------------------------------------------------
 
   do {
     var use_lowercase = confirm("Include lowercase characters in password?");
     var use_uppercase = confirm("Include uppercase characters in password?");
     var use_numeric   = confirm("Include numeric characters in password?");
     var use_special   = confirm("Include special characters in password?");
-/*
-    use_lowercase = use_lowercase.toLowerCase;
-    use_uppercase = use_uppercase.toLowerCase;
-    use_numeric   = use_numeric.toLowerCase;
-    use_special   = use_special.toLowerCase;
 
-    if (use_lowercase === 'y' || use_uppercase === 'y' || use_numeric === 'y' || use_special === 'y') {
-*/
     if (use_lowercase || use_uppercase || use_numeric || use_special ) {
       keep_looping = false;
     }
@@ -60,41 +64,66 @@ function generatePassword() {
 
   console.log(use_lowercase, use_uppercase, use_numeric, use_special);
 
-
+  // ---------------------------------------------------
   // create a new password based on the user's criteria
+  // ---------------------------------------------------
 
   // initialize new pswd to empty string
-    var new_pswd = "";
+    var new_password = "";
 
   //  define next char to be appended to pswd
     var next_char;
 
+  // create new password by appending characters until we have reached the user's desired length
     for (let i = 0; i < pswd_length; i++) {
       
-      if (use_lowercase) {
-         next_char = lowercase_char[Math.floor(Math.random() * 26)];
-      }
-      
-      if (use_uppercase) {
-        next_char = uppercase_char[Math.floor(Math.random() * 26)];
-      } 
-      
-      if (use_numeric) {
-        next_char = numeric_char[Math.floor(Math.random() * 10)];
-      } 
-      
-      if (use_special) {
-        next_char = special_char[Math.floor(Math.random() * 31)];
-      }
-      
-      new_pswd = new_pswd + next_char;
+      // call function to get next eligible character based on user's criteria
+      next_char = nextChar(use_lowercase, use_uppercase, use_numeric, use_special);
+
+      new_password = new_password + next_char;
 
     }
 
-    console.log(pswd);
+    console.log(new_password);
+
+    return new_password;
 
 }
 
+// --------------------------------------------------------
+// Function to determine next character for new password
+// --------------------------------------------------------
+
+function nextChar (use_lower, use_upper, use_num, use_spcl) {
+
+  var nxt_chr = "";
+
+  // Loop until we find the next valid random character based on user's criteria
+
+  do {
+
+    var select_random = Math.floor(Math.random() * 4);
+
+    if (use_lower && select_random === 0) {
+      nxt_chr = lowercase_char[Math.floor(Math.random() * 26)];
+    }
+  
+    if (use_upper && select_random === 1) {
+      nxt_chr = uppercase_char[Math.floor(Math.random() * 26)];
+    } 
+  
+    if (use_num && select_random === 2) {
+      nxt_chr = numeric_char[Math.floor(Math.random() * 10)];
+    }  
+  
+    if (use_spcl && select_random === 3) {
+      nxt_chr = special_char[Math.floor(Math.random() * 29)];
+    }
+
+  } while (nxt_chr === "");
+
+  return nxt_chr;
+}
 
 
 // Write password to the #password input
